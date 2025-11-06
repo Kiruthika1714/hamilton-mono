edb522a9a69957557ae5bcbce3793127418572d7
kirthu25 <kirthu25@gmail.com>
2025-11-06 13:07:45 +0530

    Added billing-service, driver-service, and vehicle-service with CI workflow

---
+++ billing-service/server.js
@@ -0,0 +1,5 @@
+console.log("Billing service running..."); 
+let totalBills = 120; 
+let revenue = 54000; 
+console.log(`Total bills processed: ${totalBills}`); 
+console.log(`Total revenue generated: ₹${revenue}`); 
