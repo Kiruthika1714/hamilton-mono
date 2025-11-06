17cd1c1e54bfc23dc029bb42e09c542df83d5ca6
kirthu25 <kirthu25@gmail.com>
2025-11-06 13:04:52 +0530

    Prepare billing-service for submodule

--- billing-service/server.js
+++
@@ -1,5 +0,0 @@
-console.log("Billing service running..."); 
-let totalBills = 120; 
-let revenue = 54000; 
-console.log(`Total bills processed: ${totalBills}`); 
-console.log(`Total revenue generated: ₹${revenue}`); 
