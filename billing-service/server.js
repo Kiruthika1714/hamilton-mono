c02448828088e3c1a20e9fa3287364b5c92480b2
kirthu25 <kirthu25@gmail.com>
2025-11-07 14:18:25 +0530

    failure

--- billing-service/server.js
+++ billing-service/server.js
@@ -1,5 +1,7 @@
-console.log("Billing service running..."); 
-let totalBills = 120; 
-let revenue = 54000; 
-console.log(`Total bills processed: ${totalBills}`); 
-console.log(`Total revenue generated: ₹${revenue}`); 
+console.log("Billing service running...");
+
+let totalBills = 120;
+let revenue = 54000;
+
+console.log(`Total bills processed: ${totalBills}`);
+console.log(`Total revenue generated: ₹${revenue}`);
